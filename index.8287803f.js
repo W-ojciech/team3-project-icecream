c877df3201f643828b1ac416f06bd270927a8046
W-ojciech <58496148+W-ojciech@users.noreply.github.com>
2023-09-11 13:34:34 +0000

    Deploying to gh-pages from @ W-ojciech/team3-project-icecream@fe6f7b7756b92d6b3d409585dc3d2ec408cbaeef 🚀

--- index.8287803f.js
+++
@@ -1,2 +0,0 @@
-(()=>{const o={openModalBtn:document.querySelector("[buy-now-modal-open]"),closeModalBtn:document.querySelector("[buy-now-modal-close]"),modal:document.querySelector("[buy-now-modal]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
-//# sourceMappingURL=index.8287803f.js.map
